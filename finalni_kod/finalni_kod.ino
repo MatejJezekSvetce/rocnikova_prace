66f300fdae0f670819790d67f122c65849595cb7
Matěj Ježek <73279945+MatejJezekSvetce@users.noreply.github.com>
2021-05-16 15:23:01 +0200

    Add files via upload

--- finalni_kod/finalni_kod.ino
+++ finalni_kod/finalni_kod.ino
@@ -8,13 +8,15 @@ String myPORT = "80";
 String myFIELD = "field1"; 
 String myFIELD2 = "field2";
 String myFIELD3 = "field3";  
+String myFIELD4 = "field4";
 int sendVal;
 int sendVal2;
 int sendVal3;
+int sendVal4;
 //------------------------------------------------------------------
 //DHT
 #include "DHT.h"
-#define pinDHT 6
+#define pinDHT 10
 #define typDHT22 DHT22 
 DHT mojeDHT(pinDHT, typDHT22);
 //------------------------------------------------------------------
@@ -22,9 +24,12 @@ DHT mojeDHT(pinDHT, typDHT22);
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp180;
-int korekce = 58.6;
+int korekce = 1003;
 
 //------------------------------------------------------------------
+//anemo
+int sensorValue;
+
 
 void setup()
 {
@@ -43,9 +48,9 @@ void setup()
   espData("AT+CIFSR", 1000, DEBUG);
 //----------------------------------------------------------------
 
- Serial.begin(9600); 
  mojeDHT.begin();
  bmp180.begin();
+ //-----------------------------
 
 
   
@@ -58,28 +63,17 @@ void setup()
   float tep = mojeDHT.readTemperature();
   float vlh = mojeDHT.readHumidity();
   
-  if (isnan(tep) || isnan(vlh)) {
-
-     Serial.println("------------------------------");
-    Serial.println("Chyba při čtení z DHT senzoru!");
-     Serial.println("------------------------------");
-  } else {
-    
-    Serial.println("------------------------------");
-    Serial.print("Teplota: "); 
-    Serial.print(tep);
-
-    Serial.print("vlhkost: "); 
-    Serial.print(vlh);
-
-  }
   
- Serial.print("Barometricky tlak: ");
+
  float tlk = bmp180.readPressure()/100 + korekce;
- Serial.print(tlk);
+
 
 
  delay(5000);
+ //--------------------------------------------
+  //Anemo
+ sensorValue = analogRead(A1);
+ Serial.println(sensorValue);
 
  //-----------------------------------------------------------------------
 
@@ -87,17 +81,15 @@ void setup()
     sendVal = tep; // Send a random number between 1 and 1000
     sendVal2 = tlk;
     sendVal3 = vlh;
+    sendVal4 = sensorValue;
     //http://api.thingspeak.com/update?api_key=XVKHSUERMX3UPQD9&field1=73
-    String sendData = "GET /update?api_key="+ myAPI +"&"+ myFIELD +"="+String(sendVal)+"&"+ myFIELD2 +"="+String(sendVal2)+"&"+ myFIELD3 +"="+String(sendVal3);
+    String sendData = "GET /update?api_key="+ myAPI +"&"+ myFIELD +"="+String(sendVal)+"&"+ myFIELD2 +"="+String(sendVal2)+"&"+ myFIELD3 +"="+String(sendVal3)+"&"+ myFIELD4 +"="+String(sendVal4);
     espData("AT+CIPMUX=1", 1000, DEBUG);       //Allow multiple connections
     espData("AT+CIPSTART=0,\"TCP\",\""+ myHOST +"\","+ myPORT, 1000, DEBUG);
     espData("AT+CIPSEND=0," +String(sendData.length()+2),1000,DEBUG);  
     Serial1.find(">"); 
     Serial1.println(sendData);
-    Serial.print("Value to be sent: ");
-    Serial.println(sendData);
-    Serial.print("size to be send: ");
-    Serial.println(sendData.length());
+   
      
     espData("AT+CIPCLOSE=0",1000,DEBUG);
     delay(6000);
@@ -105,9 +97,7 @@ void setup()
 
   String espData(String command, const int timeout, boolean debug)
 {
-  Serial.print("AT Command ==> ");
-  Serial.print(command);
-  Serial.println("     ");
+  
   
   String response = "";
   Serial1.println(command);
@@ -121,7 +111,7 @@ void setup()
     }
   }
 
-    Serial.print(response);
+    
 
   return response;
 }
